06ad28449ed88a87e83a4f49ed2bd2d44e7cf75b
Michael Mykhaylov <32168861+mikemykhaylov@users.noreply.github.com>
2026-04-11 15:07:37 -0400

    Don't auto-install parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,14 +13,11 @@ return {
     treesitter = {
       highlight = true, -- enable/disable treesitter based highlighting
       indent = true, -- enable/disable treesitter based indentation
-      auto_install = true, -- enable/disable automatic installation of detected languages
+      auto_install = false, -- enable/disable automatic installation of detected languages
       ensure_installed = {
         "lua",
         "vim",
         -- add more arguments for adding more treesitter parsers
-      },
-      ignore_install = {
-        "gitcommit",
       },
     },
   },
